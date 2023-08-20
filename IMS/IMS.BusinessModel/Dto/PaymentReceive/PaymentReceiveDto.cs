3ca76b760e0dbcad2cd90fc3172c63a9c74bc328
musarratChowdhury <muhitlopez143@gmail.com>
2023-08-20 12:46:11 +0600

    an addition to the previous commit

--- IMS/IMS.BusinessModel/Dto/PaymentReceive/PaymentReceiveDto.cs
+++ IMS/IMS.BusinessModel/Dto/PaymentReceive/PaymentReceiveDto.cs
@@ -1,7 +1,23 @@
-﻿namespace IMS.BusinessModel.Dto.PaymentReceive
+﻿using System;
+using IMS.BusinessModel.Dto.CommonDtos;
+
+namespace IMS.BusinessModel.Dto.PaymentReceive
 {
-    public class PaymentReceiveDto
+    public class PaymentReceiveDto : BaseDto
     {
-        
+        public DateTime PaymentDate { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public long? InvoiceId { get; set; }
+        public string InvoiceSerialNumber { get; set; }
+        public long? PaymentTypeId { get; set; }
+        public string PaymentTypeName { get; set; }
+    }
+
+    public class PaymentReceiveFormDto
+    {
+        public DateTime PaymentDate { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public long? InvoiceId { get; set; }
+        public long? PaymentTypeId { get; set; }
     }
 }
