95540d10fc10868080b0563514aa7a6dea9fddd2
musarratChowdhury <muhitlopez143@gmail.com>
2023-08-21 16:44:57 +0600

    PurchaseOrderDto code added

--- IMS/IMS.BusinessModel/Dto/PurchaseOrder/PurchaseOrderDto.cs
+++ IMS/IMS.BusinessModel/Dto/PurchaseOrder/PurchaseOrderDto.cs
@@ -1,7 +1,29 @@
-﻿namespace IMS.BusinessModel.Dto.PurchaseOrder
+﻿using System.Collections.Generic;
+using IMS.BusinessModel.Entity.OrderDetails;
+
+namespace IMS.BusinessModel.Dto.PurchaseOrder
 {
     public class PurchaseOrderDto
     {
+        public string SerialNumber { get; set; }
+        public int PaymentStatus { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsArchived { get; set; }
+
+        // Navigation property for the foreign key
+        public long VendorId { get; set; }
+        public string VendorName { get; set; }
+        public long? BillId { get; set; }
+        public string BillSerialNumber { get; set; }
+
+        public List<PurchaseOrderLine> PurchaseOrderLines { get; set;}
+    }
+
+    public class PurchaseOrderFormDto
+    {
+        public long VendorId { get; set; }
+        public decimal TotalAmount { get; set; }
         
+        public List<PurchaseOrderLine> PurchaseOrderLines { get; set; }
     }
 }
